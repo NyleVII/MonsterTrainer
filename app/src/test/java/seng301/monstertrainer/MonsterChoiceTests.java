e59cbac8d8bdf767dd9beb4f23be09c4e4d7c8a9
Alex Thompson <alexcthomp@gmail.com>
2016-03-10 23:25:16 -0700

    test file added you can pass or fail it if you wish

---
+++ app/src/test/java/seng301/monstertrainer/MonsterChoiceTests.java
@@ -0,0 +1,42 @@
+package seng301.monstertrainer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Some tests that can pass or fail
+ */
+public class MonsterChoiceTests {
+
+    @Test
+    public void chooseOgreTest() throws Exception {
+        assertEquals("OGRE","");
+    }
+
+    @Test
+    public void chooseTrollTest() throws Exception {
+        assertEquals("TROLL","");
+    }
+
+    @Test
+    public void chooseGoblinTest() throws Exception {
+        assertEquals("GOBLIN","");
+    }
+
+    @Test
+    public void chooseImpTest() throws Exception {
+        assertEquals("IMP","");
+    }
+
+    @Test
+    public void chooseOrcTest() throws Exception {
+        assertEquals("ORC","");
+    }
+
+    @Test
+    public void chooseGremlinTest() throws Exception {
+        assertEquals("GREMLIN","");
+    }
+
+}
